3017ce3c4d7094817d5d374701af49730cdbec3f
Lulezer <150733591+Lulezer@users.noreply.github.com>
2025-02-20 04:29:22 -0800

    Update main.cpp

--- Examples/Audio/PlayAudio/main.cpp
+++ Examples/Audio/PlayAudio/main.cpp
@@ -10,31 +10,31 @@ int main(void)
     // Load sound files
     Sound fxWav = LoadSound("song.wav");  // Load sound file (change extension if needed)
 
-    MakeWindow(screenWidth, screenHeight, "raylib [audio] example - sound loading and playing");
+    ech::MakeWindow(screenWidth, screenHeight, "raylib [audio] example - sound loading and playing");
 
     InitAudioDevice();  // Initialize audio device
 
     // Set target FPS
-    SetTargetFps(60);
+    ech::SetTargetFps(60);
 
     PlaySound(fxWav);
 
 
     // Main game loop
-    while (!WindowShouldClose()) {  // Detect window close button or ESC key
+    while (!ech::WindowShouldClose()) {  // Detect window close button or ESC key
 
         // Draw
-        StartDrawing();
+        ech::StartDrawing();
 
-        ClearBackground(WHITE);
+        ech::ClearBackground(ech::WHITE);
 
-        EndDrawing();
+        ech::EndDrawing();
     }
 
     // De-initialization
     UnloadSound(fxWav);  // Unload sound data
     CloseAudioDevice();   // Close audio device
-    CloseWindow();        // Close window and OpenGL context
+    ech::CloseWindow();        // Close window and OpenGL context
 
     return 0;
 }
